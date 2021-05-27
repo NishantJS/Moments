1ee22935b0a940d83c746b361e4c53f38f2fb21e
NishantJS <nishantchorge79@gmail.com>
2021-05-27 20:25:59 +0530

    Firestore chat fixes

--- src/view/components/Chats.jsx
+++ src/view/components/Chats.jsx
@@ -2,16 +2,36 @@ import {
   ChatContainer,
   MessageList,
   Message,
-  Avatar,
+  Avatar,MessageInput,
   ConversationHeader
 } from "@chatscope/chat-ui-kit-react";
-import { firestore, useAuthHook, useCollections } from "../services/firebase.js";
-import InputMessage from "./InputMessage.jsx";
+import {
+  firestore,
+  useAuthHook,
+  useCollections,
+  serverStore,
+} from "../services/firebase.js";
+import { useState } from "react";
 
 const Chats = ({ email, displayName, photoURL, updateChatScreen }) => {
   const [username,] = email.split("@");
   const [firstname, lastname] = displayName.split(" ");
   const [user] = useAuthHook();
+  const [messageInputValue, setMessageInputValue] = useState("");
+
+  const sendMessage = async (value) => {
+    if (value.length < 255) {
+      await messageRef.add({
+        text: value,
+        createAt: serverStore.serverTimestamp(),
+        user: me,
+      });
+
+      setMessageInputValue("");
+    } else {
+      setMessageInputValue(value);
+    }
+  };
 
   const getInitials = () => {
     let initials = "";
@@ -32,7 +52,7 @@ const Chats = ({ email, displayName, photoURL, updateChatScreen }) => {
   const messageRef = firestore.collection(docName);
 
   const query = messageRef.orderBy("createAt")
-    .limit(10);
+    .limit(15);
 
   const [messages] = useCollections(docName, query);
     
@@ -44,20 +64,18 @@ const Chats = ({ email, displayName, photoURL, updateChatScreen }) => {
       if (index >= 1) {
         let prevDate = messages[index - 1].createAt.toDate().toLocaleDateString();
 
-        if (day === prevDate) return `${time}`
-        else {
-          if (day === new Date().toLocaleDateString()) {
-            if (time !== new Date().toLocaleTimeString()) {
-              return `today ${time}`;
-            } else {
-              return `just now`;
-            }
-          }
-            else return `${day} ${time}`;
+        if (day !== prevDate) {
+          return `${day} ${time}`;
+        } else {
+          return time;
         }
+          
       }
+      else {
+        return `${date.toLocaleString()}`;
+      };
     }
-    return `${day} ${time}`;
+    return;
   }
 
   return (
@@ -72,33 +90,43 @@ const Chats = ({ email, displayName, photoURL, updateChatScreen }) => {
         <ConversationHeader.Actions></ConversationHeader.Actions>
       </ConversationHeader>
       <MessageList
-        style={{ height: "100%", marginTop: "10px", overflow: "hidden"}}
+        style={{ height: "100%", marginTop: "10px", overflow: "hidden" }}
       >
-          {messages &&
-            messages.map((message, index) => {
-              return (
-                <Message
-                  model={{
-                    message: message.text,
-                    sentTime: "",
-                    sender: message.user === me ? "Me" : message.user,
-                    direction: message.user !== me ? "incoming" : "outgoing",
-                    position: "single",
-                  }}
-                >
-                  {" "}
-                  <Avatar src={photoURL} name={getInitials()} />
-                  <Message.Footer
-                    sentTime={
-                      message.createAt &&
-                      getMessageTime(message.createAt.toDate(), index)
-                    }
-                  />
-                </Message>
-              );
-            })}
-        <InputMessage messageRef={messageRef} me={me} />
+        {messages &&
+          messages.map((message, index) => {
+            return (
+              <Message
+                model={{
+                  message: message.text,
+                  sentTime: "",
+                  sender: message.user === me ? "Me" : message.user,
+                  direction: message.user !== me ? "incoming" : "outgoing",
+                  position: "single",
+                }}
+                key={message.text + index}
+              >
+                {" "}
+                <Avatar src={photoURL} name={getInitials()} />
+                <Message.Footer
+                  sentTime={
+                    message.createAt &&
+                    getMessageTime(message.createAt.toDate(), index)
+                  }
+                />
+              </Message>
+            );
+          })}
+        {/* <InputMessage messageRef={messageRef} me={me} />
+        000 */}
       </MessageList>
+      <MessageInput
+        style={{ bottom: "0", right: "0", left: "0", width: "100%" }}
+        placeholder="Type message here"
+        value={messageInputValue}
+        onChange={(value) => setMessageInputValue(value)}
+        onSend={(value) => sendMessage(value)}
+        sendDisabled={messageInputValue.length < 255 ? false : true}
+      />
     </ChatContainer>
   );
 }
